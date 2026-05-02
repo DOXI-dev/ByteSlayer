92a7ea611037a576365aaf9708bcecaa0bc73c84
DOXI-dev <kiliansamfan@gmail.com>
2026-05-02 23:10:51 +0200

    Add keep_alive function to lichess-bot

--- lichess-bot.py
+++ lichess-bot.py
@@ -1,5 +1,8 @@
 """Starting point for lichess-bot."""
 from lib.lichess_bot import start_program
+from keep_alive import keep_alive
 
 if __name__ == "__main__":
+    keep_alive()
     start_program()
+    
