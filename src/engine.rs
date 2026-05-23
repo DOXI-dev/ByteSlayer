af38996e70b5e2c85606029d9cc2a3e6cb85f203
DOXI-dev <kiliansamfan@gmail.com>
2026-05-23 22:06:44 +0200

    Corrected critical bugs

--- src/engine.rs
+++ src/engine.rs
@@ -7,10 +7,21 @@ use crate::evaluation;
 use chess::{BitBoard, Board, BoardStatus, ChessMove, MoveGen, Piece};
 
 // ----- MODEL -----
+// Function to avoid duplicating piece values
+fn get_piece_value(piece: Piece) -> i32 {
+    match piece {
+        Piece::Pawn => 100,
+        Piece::Knight => 320,
+        Piece::Bishop => 330,
+        Piece::Rook => 500,
+        Piece::Queen => 900,
+        Piece::King => 20000,
+    }
+}
+
 // Move sorting for the negamax function
 fn sort_moves(board: &Board, movegen: MoveGen) -> Vec<ChessMove> {
     let mut moves: Vec<ChessMove> = movegen.collect();
-
     let mut targets = *board.color_combined(!board.side_to_move());
 
     if let Some(ep_sq) = board.en_passant() {
@@ -19,32 +30,15 @@ fn sort_moves(board: &Board, movegen: MoveGen) -> Vec<ChessMove> {
 
     moves.sort_by_key(|m| {
         let mut score = 0;
-
         let destination = m.get_dest();
-
         let source = m.get_source();
-
         let dest_mask = BitBoard::from_square(destination);
 
         if (targets & dest_mask).popcnt() > 0 {
             if let Some(capturing_piece) = board.piece_on(source) {
                 if let Some(captured_piece) = board.piece_on(destination) {
-                    let captured_value = match captured_piece {
-                        Piece::Pawn => 100,
-                        Piece::Knight => 320,
-                        Piece::Bishop => 330,
-                        Piece::Rook => 500,
-                        Piece::Queen => 900,
-                        Piece::King => 20000,
-                    };
-                    let capturing_value = match capturing_piece {
-                        Piece::Pawn => 100,
-                        Piece::Knight => 320,
-                        Piece::Bishop => 330,
-                        Piece::Rook => 500,
-                        Piece::Queen => 900,
-                        Piece::King => 20000,
-                    };
+                    let captured_value = get_piece_value(captured_piece);
+                    let capturing_value = get_piece_value(capturing_piece);
 
                     score += 1000 + (captured_value - capturing_value);
                 } else {
@@ -58,8 +52,7 @@ fn sort_moves(board: &Board, movegen: MoveGen) -> Vec<ChessMove> {
 }
 
 // Quiescence search function
-fn quiescence(board: &Board, alpha: i32, beta: i32) -> i32 {
-    let mut alpha = alpha;
+fn quiescence(board: &Board, mut alpha: i32, beta: i32) -> i32 {
     let static_eval = evaluation::evaluation(board);
 
     if static_eval >= beta {
@@ -68,25 +61,20 @@ fn quiescence(board: &Board, alpha: i32, beta: i32) -> i32 {
     if static_eval > alpha {
         alpha = static_eval;
     }
-    let movegen = MoveGen::new_legal(board);
+    let mut movegen = MoveGen::new_legal(board);
+    movegen.set_iterator_mask(*board.color_combined(!board.side_to_move()));
 
     let sorted_moves = sort_moves(board, movegen);
 
     for m in sorted_moves {
-        let is_capture = board.piece_on(m.get_dest()).is_some();
-        let is_en_passant = Some(m.get_dest()) == board.en_passant();
+        let new_board = board.make_move_new(m);
+        let score = -quiescence(&new_board, -beta, -alpha);
 
-        if is_capture || is_en_passant {
-            let new_board = board.make_move_new(m);
-
-            let score = quiescence(&new_board, -beta, -alpha);
-
-            if score >= beta {
-                return beta;
-            }
-            if score > alpha {
-                alpha = score;
-            }
+        if score >= beta {
+            return beta;
+        }
+        if score > alpha {
+            alpha = score;
         }
     }
     alpha
@@ -97,20 +85,18 @@ const INFINITY: i32 = 2_000_000;
 
 // Negamax and alpha-beta pruning function
 fn negamax(board: &Board, depth: i32, mut alpha: i32, beta: i32) -> i32 {
-    let movegen = MoveGen::new_legal(board);
+    match board.status() {
+        BoardStatus::Checkmate => return -20000 + depth,
+        BoardStatus::Stalemate => return 0,
+        BoardStatus::Ongoing => {}
+    }
 
-    let new_movegen = sort_moves(board, movegen);
-
-    if depth == 0 || new_movegen.len() == 0 {
-        if board.status() == BoardStatus::Checkmate {
-            return -20000 + depth;
-        }
-        if board.status() == BoardStatus::Stalemate {
-            return 0;
-        }
-
+    if depth == 0 {
         return quiescence(board, alpha, beta);
     }
+
+    let movegen = MoveGen::new_legal(board);
+    let new_movegen = sort_moves(board, movegen);
 
     let mut value: i32 = -INFINITY;
 
@@ -131,20 +117,20 @@ fn negamax(board: &Board, depth: i32, mut alpha: i32, beta: i32) -> i32 {
 
 // Function to choose a move
 pub fn choose_move(depth: i32, board: &Board) -> Option<ChessMove> {
-    let mut best_value: i32 = -INFINITY;
     let mut best_move: Option<ChessMove> = None;
+    let mut alpha = -INFINITY;
+    let beta = INFINITY;
 
     let movegen = MoveGen::new_legal(board);
-
     let new_movegen = sort_moves(board, movegen);
 
     for m in new_movegen {
         let new_board = board.make_move_new(m);
 
-        let value = -negamax(&new_board, depth - 1, -INFINITY, INFINITY);
+        let value = -negamax(&new_board, depth - 1, -beta, -alpha);
 
-        if value > best_value {
-            best_value = value;
+        if value > alpha {
+            alpha = value;
             best_move = Some(m);
         }
     }
